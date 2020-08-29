d935fba8fe6bba8315a02f71e4bda6b3a9b6a251
taki4227 <takiki.truth@gmail.com>
2020-08-29 16:16:04 +0900

    Use data binding

--- app/src/main/java/com/example/taki/web_socket_sample/MainActivity.kt
+++ app/src/main/java/com/example/taki/web_socket_sample/MainActivity.kt
@@ -2,10 +2,15 @@ package com.example.taki.web_socket_sample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.example.taki.web_socket_sample.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private val binding by lazy {
+        DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
     }
 }
