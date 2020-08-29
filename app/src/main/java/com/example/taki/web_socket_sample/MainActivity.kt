30569a1f21e726d0851311d9f6fa6338963c7c84
taki4227 <takiki.truth@gmail.com>
2020-08-29 17:25:47 +0900

    Implement okhttp web socket

--- app/src/main/java/com/example/taki/web_socket_sample/MainActivity.kt
+++ app/src/main/java/com/example/taki/web_socket_sample/MainActivity.kt
@@ -7,6 +7,9 @@ import androidx.databinding.DataBindingUtil
 import com.example.taki.web_socket_sample.databinding.ActivityMainBinding
 import io.socket.client.IO
 import io.socket.client.Socket
+import okhttp3.*
+import okio.ByteString
+import java.util.concurrent.TimeUnit
 
 class MainActivity : AppCompatActivity() {
     private val binding by lazy {
@@ -24,6 +27,16 @@ class MainActivity : AppCompatActivity() {
         socket
     }
 
+    private val okHttpClient by lazy {
+        OkHttpClient.Builder()
+            .connectTimeout(10, TimeUnit.SECONDS)
+            .readTimeout(10, TimeUnit.SECONDS)
+            .writeTimeout(10, TimeUnit.SECONDS)
+            .build()
+    }
+    private var webSocket: WebSocket? = null
+    private var isConnect = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -36,12 +49,61 @@ class MainActivity : AppCompatActivity() {
                 socket.emit("event_name", "Send text")
             }
         }
+
+        binding.socketIoConnectButton.setOnClickListener {
+            val request = Request.Builder().url("url").build()
+            webSocket = okHttpClient.newWebSocket(request, object : WebSocketListener() {
+                override fun onOpen(webSocket: WebSocket, response: Response) {
+                    super.onOpen(webSocket, response)
+                    Log.d(TAG, "onOpen")
+                    isConnect = true
+                }
+
+                override fun onMessage(webSocket: WebSocket, text: String) {
+                    super.onMessage(webSocket, text)
+                    Log.d(TAG, "onMessage string")
+                }
+
+                override fun onMessage(webSocket: WebSocket, bytes: ByteString) {
+                    super.onMessage(webSocket, bytes)
+                    Log.d(TAG, "onMessage byte")
+                }
+
+                override fun onClosing(webSocket: WebSocket, code: Int, reason: String) {
+                    super.onClosing(webSocket, code, reason)
+                    Log.d(TAG, "onClosing")
+                }
+
+                override fun onClosed(webSocket: WebSocket, code: Int, reason: String) {
+                    super.onClosed(webSocket, code, reason)
+                    Log.d(TAG, "onClosed")
+                    isConnect = false
+                }
+
+                override fun onFailure(webSocket: WebSocket, t: Throwable, response: Response?) {
+                    super.onFailure(webSocket, t, response)
+                    Log.e(TAG, "onFailure", t)
+                }
+            })
+
+            okHttpClient.dispatcher.executorService.shutdown()
+        }
+
+        binding.socketIoSendButton.setOnClickListener {
+            if (isConnect) {
+                webSocket?.send("send text")
+            }
+        }
     }
 
     override fun onDestroy() {
         socket.off()
         if (socket.connected()) {
             socket.disconnect()
+        }
+        if (isConnect) {
+            // status code see: https://tools.ietf.org/html/rfc6455#section-7.4
+            webSocket?.close(1000, "connection close")
         }
         super.onDestroy()
     }
